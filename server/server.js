e43cbfc37372c644f5953424139eb86cf2300e5f
Donovan <donovanandrews300@gmail.com>
2020-01-13 11:34:07 -0500

    Added api resurce retrival

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', function (req, res) {
     const books = ['Deathly Hollows', 'Name of the Wind', 'Golden Compass', 'Way of Kings'];
