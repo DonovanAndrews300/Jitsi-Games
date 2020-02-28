878a1609a6b65bf68958b3382ddc421ddc519f20
Aaron van Meerten <aaron.van.meerten@gmail.com>
2020-02-28 09:08:11 -0600

    eslint fixes for server

--- server/server.js
+++ server/server.js
@@ -1,35 +1,37 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const redis = require("redis");
-const cors = require("cors");
+const redis = require('redis');
+const cors = require('cors');
 const port = process.env.PORT;
 
-let client = redis.createClient();
-client.on("connect", function() {
-  console.log("Redis is connected");
+const client = redis.createClient();
+
+client.on('connect', () => {
+    console.log('Redis is connected');
 });
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", function(req, res) {
-  //gets from db
-  client.lrange("domains", 0, -1, function(err, reply) {
-    res.json(reply.toString());
-  });
-  console.log(res.body);
+app.get('/', (req, res) => {
+    // gets from db
+    client.lrange('domains', 0, -1, (err, reply) => {
+        res.json(reply.toString());
+    });
+    console.log(res.body);
 });
 
-app.post("/", function(req, res) {
-  //adds to db
-  const body = req.body.data;
-  client.lpush("domains", body, function(err, reply) {
-    console.log(reply);
-    res.json(reply);
-  });
+app.post('/', (req, res) => {
+    // adds to db
+    const body = req.body.data;
+
+    client.lpush('domains', body, (err, reply) => {
+        console.log(reply);
+        res.json(reply);
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+    console.log(`Running on port ${port}`);
 });
