c07426d03e9f8b4246189bab5bef2f1d6010c692
Donovan <donovanandrews300@gmail.com>
2020-05-10 20:25:20 -0400

    changes for deployment2 json

--- server/server.js
+++ server/server.js
@@ -46,10 +46,10 @@ app.post('/gameState', (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname, 'dist'));
+    app.use(express.static('client/build'));
     
     app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
