04e8fce301739b3b23cae36aa783ee83f8c9a5a8
Donovan <donovanandrews300@gmail.com>
2020-05-17 15:16:25 -0400

    test3

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const port = process.env.PORT;
 
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8000 });
+const wss = new WebSocket.Server({ port });
 
 const client = redis.createClient(process.env.REDIS_URL);
 
@@ -17,20 +17,20 @@ client.on('connect', () => {
     console.log('Redis is connected');
 });
 
-app.use(express.static(__dirname + '/client/dist'))
+app.use(express.static(`${__dirname}/client/dist`));
 app.use(bodyParser.json());
 app.use(cors());
 
 wss.on('connection', ws => {
     ws.on('message', message => {
-        console.log(`Sednings to clients:${message}`);
+        console.log(`Sending to clients:${message}`);
         wss.broadcast(message);
     });
 });
 
 wss.broadcast = msg => {
     wss.clients.forEach(socketClient => socketClient.send(msg));
-}
+};
 
 app.get('/gameRoom', (req, res) => {
     // gets list of gameRoom objects from the database
