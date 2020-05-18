98366028dc528c7e3fde86988f0b627f0de22583
Donovan <donovanandrews300@gmail.com>
2020-05-18 19:53:35 -0400

    test32

--- server/server.js
+++ server/server.js
@@ -7,10 +7,7 @@ const cors = require('cors');
 const port = process.env.PORT;
 
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({
-    port:8000,
-    host:"ws://jitsigame.herokuapp.com"
-});
+const wss = new WebSocket.Server({port:8000});
 
 const client = redis.createClient(process.env.REDIS_URL);
 
