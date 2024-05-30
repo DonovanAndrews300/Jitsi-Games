ebc2263dbd623e07c35316e2f20a59fb6dda0f1f
Donovan <donovanandrews300@gmail.com>
2024-05-30 19:47:04 -0400

    can dynamically load in games

--- client/src/js/gameContainer.js
+++ client/src/js/gameContainer.js
@@ -1,7 +1,8 @@
+import TicTacToe from '../js/games/TicTacToe';
 import DataClient from './DataClient';
 import { config } from './config';
 
-window.copyURL= function() {
+window.copyURL = function() {
     const url = window.location.href;
     navigator.clipboard.writeText(url).then(() => {
         alert('URL copied to clipboard!');
@@ -18,23 +19,37 @@ function getUrlParams() {
     return {gameId, gameType};
 }
 
-function join() {
-  
-    const {gameId, gameType} = getUrlParams(); 
+function joinGame() {
+    const { gameId, gameType } = getUrlParams(); 
     console.log(gameId);
     const userId = Math.random().toString(36).substr(2, 9);
     const _dataClient = new DataClient(config.apiUrl,config.wsUrl);
     _dataClient.joinGame(gameId, userId).then(() => {
         alert("Game joined successfully");
+        injectGame(gameId, userId,gameType,_dataClient);
     }).catch((err) => alert(err));
+    
     window.addEventListener('beforeunload', (event) => {
-        _dataClient.leaveGame(gameId,userId).then(() =>{
+        _dataClient.leaveGame(gameId, userId).then(() => {
             console.log("Game left successfully");
-        })
-    })
+        }).catch((err) => console.error("Error leaving game:", err));
+    });
 }
 
+function injectGame(gameId, userId,gameType, dataClient) {
+    const gameRoom = { name: gameId, playerSession: userId };
+    const ticTacToeGame = new TicTacToe(gameRoom, dataClient);
+    const link = document.createElement('link');
+    import(`../js/games/${gameType}`)
+        .then(module => {
+            const Game = module.default;
+            const game = new Game();
+            game.renderGame(game);
+        });
+    link.rel = 'stylesheet';
+    link.href =`../css/${gameType}.css` ;
+    document.head.appendChild(link);
+}
 
-
-
-join();
+// Call joinGame when the script loads
+joinGame();
