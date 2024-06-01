15418ec965bce60b1eacd562a366a5e05617235f
Donovan <donovanandrews300@gmail.com>
2024-06-01 10:45:19 -0400

    added env for port

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const websocket = require('./websocket');
 const routes = require('./routes');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
