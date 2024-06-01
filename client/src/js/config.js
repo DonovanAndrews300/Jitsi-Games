4fda119c43c68f31891b2d83056e2fb0982aaea4
Donovan <donovanandrews300@gmail.com>
2024-06-01 19:35:27 -0400

    attemp fix url

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,4 +1,4 @@
 export const config = {
     apiUrl:'https://jitsigames-2e12a109cdf9.herokuapp.com/',
-    wsUrl:"wss://https://jitsigames-2e12a109cdf9.herokuapp.com/"
+    wsUrl:location.origin.replace(/^http/, 'ws').replace(/^https/, 'wss'),
 }
