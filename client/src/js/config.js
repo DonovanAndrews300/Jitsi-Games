00e5d09e2289ec33b98ee6da86cedfd6b5ce7dfe
Donovan <donovanandrews300@gmail.com>
2024-06-02 11:09:11 -0400

    change from ws to ws

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,4 +1,4 @@
 export const config = {
     apiUrl:'https://jitsigames-2e12a109cdf9.herokuapp.com/',
-    wsUrl:'ws://jitsigames-2e12a109cdf9.herokuapp.com',
+    wssUrl:'ws://jitsigames-2e12a109cdf9.herokuapp.com',
 }
