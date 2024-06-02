cf41ffb61f7b811b96761944df8509cbbabdb2f7
Donovan <donovanandrews300@gmail.com>
2024-06-02 11:11:05 -0400

    to wss

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,4 +1,4 @@
 export const config = {
     apiUrl:'https://jitsigames-2e12a109cdf9.herokuapp.com/',
-    wssUrl:'ws://jitsigames-2e12a109cdf9.herokuapp.com',
+    wsUrl:'wss://jitsigames-2e12a109cdf9.herokuapp.com',
 }
