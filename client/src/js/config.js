7a693ffd7171f62182ad9f58c5672aca34ad1305
Donovan <donovanandrews300@gmail.com>
2024-07-09 16:55:15 -0400

    remove https from wsurl

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,5 +1,5 @@
 export const config = {
     //These use secure version of the url on heroku (https,wss)
     apiUrl:'https://jitsigames-2e12a109cdf9.herokuapp.com/',
-    wsUrl:'wss:https://jitsigames-2e12a109cdf9.herokuapp.com/',
+    wsUrl:'wss://jitsigames-2e12a109cdf9.herokuapp.com/',
 }
