adbba3feed9ae39fa168e744244733bb3a95ec84
Donovan <donovanandrews300@gmail.com>
2024-07-09 16:53:18 -0400

    reverted configurls

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,5 +1,5 @@
 export const config = {
     //These use secure version of the url on heroku (https,wss)
-    apiUrl:'http://localhost:3000/',
-    wsUrl:'ws://localhost:3000/ws',
+    apiUrl:'https://jitsigames-2e12a109cdf9.herokuapp.com/',
+    wsUrl:'wss:https://jitsigames-2e12a109cdf9.herokuapp.com/',
 }
