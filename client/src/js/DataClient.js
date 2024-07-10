0564982197cb74271392679689fbad31b40e9622
Donovan <donovanandrews300@gmail.com>
2024-07-09 21:30:02 -0400

    added path back

--- client/src/js/DataClient.js
+++ client/src/js/DataClient.js
@@ -139,6 +139,7 @@ export default class DataClient {
 
             this.peer = new Peer(undefined, {
                 secure:true,
+                path:'/peerjs',
                 host: 'jitsi-game-peer-server-541f60a90bdf.herokuapp.com',
             });
 
