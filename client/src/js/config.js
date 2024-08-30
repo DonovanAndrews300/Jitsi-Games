45a40eb393b0027cd063744b43e124422c871c15
Donovan <donovanandrews300@gmail.com>
2024-08-30 16:03:55 -0400

    removed dash

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,5 +1,5 @@
 export const config = {
     //These use secure version of the url on heroku (https,wss)
-    apiUrl:'https://jitsi-games.onrender.com//',
+    apiUrl:'https://jitsi-games.onrender.com/',
     wsUrl:'wss://jitsi-games.onrender.com//',
 }
